5248bbe673f4d967de977722d2a4ed14124b6de8
ealvarezvi <e.alvarez@adigrupo.com>
2026-02-23 12:40:17 +0100

    Update montar.sh

--- montar.sh
+++ montar.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+ 
+PUNTO_MONTAJE_1="/mnt/windows/mailomya/"
+PUNTO_MONTAJE_2="/mnt/windows/tanatex/"
+ 
+sudo apt install cifs-utils -y
+ 
+echo "Por favor, introduce las credenciales para el montaje:"
 
+read -p "Usuario: " USUARIO
+
+read -s -p "Contraseña: " PASSWORD
+
+echo "" # Salto de línea después de la contraseña
+ 
+sudo mkdir -p "$PUNTO_MONTAJE_1"
+
+sudo mkdir -p "$PUNTO_MONTAJE_2"
+ 
+sudo mount -t cifs -o username="$USUARIO",password="$PASSWORD",vers=3.0 //10.0.0.101/ADJUNTOSMAILOMYA "$PUNTO_MONTAJE_1"
+ 
+sudo mount -t cifs -o username="$USUARIO",password="$PASSWORD",vers=3.0 //10.0.0.101/ADJUNTOSTANATEX "$PUNTO_MONTAJE_2"
+
+if [ $? -eq 0 ]; then
+    echo "Script de montaje ejecutado correctamente."
+else
+    echo "Hubo un error al intentar montar las unidades."
+fi
