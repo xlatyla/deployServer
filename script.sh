e8eb575ee34de3f7b4cba0a44fcb6ba7cf6b8987
ealvarezvi <e.alvarez@adigrupo.com>
2026-02-23 10:41:55 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,29 +10,24 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-echo \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
 
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
 
-sudo apt install docker-compose
-
-sudo useradd -m -s /bin/bash "docker_user"
+sudo useradd -m -s /bin/bash "docker_user" || true
 
 echo 'docker_user:docker_user' | sudo chpasswd
 
 sudo usermod -aG docker "docker_user"
- 
+
 sudo git clone "https://github.com/xlatyla/servicesADI.git" "/home/docker_user/ADIGrupo"
 
 sudo git clone "https://github.com/xlatyla/servicesSPT.git" "/home/docker_user/SPTLogistic"
+
+sudo chown -R docker_user:docker_user /home/docker_user/
 
 sudo apt install cups printer-driver-all -y
 
@@ -40,13 +35,15 @@ sudo systemctl start cups
 
 sudo systemctl enable cups
 
-sudo lpadmin -p PT_B600_SPT -E -v socket://192.168.2.244 
+sudo lpadmin -p PT_B600_SPT -E -v socket://192.168.2.244
 
 sudo lpadmin -p ES_IM3000_SPT -E -v socket://192.168.13.5
 
 sudo cupsctl --remote-any --share-printers
 
 sudo systemctl restart cups
+
+echo "Instalación completada con éxito!"
 
 
 
