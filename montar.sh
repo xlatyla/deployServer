074348003a3eac312a419d20ad0e1fab805c1a2b
ealvarezvi <e.alvarez@adigrupo.com>
2026-03-04 11:46:50 +0100

    Update montar.sh

--- montar.sh
+++ montar.sh
@@ -8,6 +8,7 @@ PUNTO_MONTAJE_5="/mnt/windows/certs"
 PUNTO_MONTAJE_6="/mnt/windows/disco_e"
 PUNTO_MONTAJE_7="/mnt/windows/FTP"
 PUNTO_MONTAJE_8="/mnt/windows/interface"
+PUNTO_MONTAJE_9="/mnt/windows/disco_c_103"
 CREDENCIALES="/root/.smbcredentials"
 
 sudo apt install cifs-utils -y
@@ -32,7 +33,7 @@ sudo mkdir -p "$PUNTO_MONTAJE_5"
 sudo mkdir -p "$PUNTO_MONTAJE_6"
 sudo mkdir -p "$PUNTO_MONTAJE_7"
 sudo mkdir -p "$PUNTO_MONTAJE_8"
-
+sudo mkdir -p "$PUNTO_MONTAJE_9"
 # 3. Añadir a fstab si no existen ya
 echo "Configurando el montaje automático en el arranque..."
 
@@ -68,14 +69,16 @@ if ! grep -q "$PUNTO_MONTAJE_8" /etc/fstab; then
     echo "//10.0.0.100/interface  $PUNTO_MONTAJE_8  cifs  credentials=$CREDENCIALES,vers=3.0,_netdev,nofail,x-systemd.requires=network-online.target 0 0" | sudo tee -a /etc/fstab > /dev/null
 fi
 
-
+if ! grep -q "$PUNTO_MONTAJE_9" /etc/fstab; then
+    echo "//10.0.0.103/c\$  $PUNTO_MONTAJE_9  cifs  credentials=$CREDENCIALES,vers=3.0,_netdev,nofail,x-systemd.requires=network-online.target 0 0" | sudo tee -a /etc/fstab > /dev/null
+fi
 
 # 4. Recargar y montar
 sudo systemctl daemon-reload
 sudo mount -a
 
 # 5. Comprobación profesional
-if mountpoint -q "$PUNTO_MONTAJE_1" && mountpoint -q "$PUNTO_MONTAJE_2" && mountpoint -q "$PUNTO_MONTAJE_3" && mountpoint -q "$PUNTO_MONTAJE_4" && mountpoint -q "$PUNTO_MONTAJE_5" && mountpoint -q "$PUNTO_MONTAJE_6" && mountpoint -q "$PUNTO_MONTAJE_7" && mountpoint -q "$PUNTO_MONTAJE_8"; then
+if mountpoint -q "$PUNTO_MONTAJE_1" && mountpoint -q "$PUNTO_MONTAJE_2" && mountpoint -q "$PUNTO_MONTAJE_3" && mountpoint -q "$PUNTO_MONTAJE_4" && mountpoint -q "$PUNTO_MONTAJE_5" && mountpoint -q "$PUNTO_MONTAJE_6" && mountpoint -q "$PUNTO_MONTAJE_7" && mountpoint -q "$PUNTO_MONTAJE_8" && mountpoint -q "$PUNTO_MONTAJE_9"; then
     echo "¡Script ejecutado correctamente! Las cinco unidades están montadas."
     ./addcron.sh
 else
